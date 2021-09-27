61b0a3880735dc17cc4dd87125aa085fd8ba6a6c
Joli Holmes <holmesjoli@gmail.com>
2021-09-27 10:58:57 -0400

    took out hardcoding of svg in index.html to that I can control width and height more easily

--- js/transition.js
+++ js/transition.js
@@ -1,9 +1,15 @@
 var position = [50, 100, 150, 200, 250, 300, 350]
 var color = ["#909cc2", "#084887", "#f58a07", "#f9ab55", "#f7f5fb", "#75f4f4", "#e2ef70"]
 
-// Add circles at the top
-d3.select("#dataviz_delay")
-.selectAll("mycircles")
+var h = 420;
+var w = 500;
+
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+svg.selectAll("mycircles")
 .data(position)
 .enter()
 .append("circle")
