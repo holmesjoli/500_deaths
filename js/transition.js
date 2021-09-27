5a3f2f3016082c997d32f4098d8e7cddffe1af89
Joli Holmes <holmesjoli@gmail.com>
2021-09-27 12:24:14 -0400

    added data

--- js/transition.js
+++ js/transition.js
@@ -1,22 +1,13 @@
 var position = [50, 100, 150, 200, 250, 300, 350]
 var color = ["#909cc2", "#084887", "#f58a07", "#f9ab55", "#f7f5fb", "#75f4f4", "#e2ef70"]
 
-var datapoints = [
-            {"x":50, "color":"#909cc2"},
-            {"x":100, "color":"#084887"},
-            {"x":150, "color":"#f58a07"},
-            {"x":200, "color":"#f9ab55"},
-            {"x":250, "color":"#f7f5fb"},
-            {"x":300, "color":"#75f4f4"},
-            {"x":350, "color":"#e2ef70"}];
-
-var data = d3.json("./data/age_data.json");
+var datapoints = [{"demo_indicator":"1-4 years","n":57,"pop_est":19767670,"one_in_x":346801,"log_one_in_x":12.7565,"color":"#03045e"},{"demo_indicator":"5-14 years","n":148,"pop_est":41084755,"one_in_x":277600,"log_one_in_x":12.5339,"color":"#0077b6"},{"demo_indicator":"15-24 years","n":1372,"pop_est":43223294,"one_in_x":31504,"log_one_in_x":10.3579,"color":"#00b4d8"},{"demo_indicator":"25-34 years","n":6054,"pop_est":45030415,"one_in_x":7438,"log_one_in_x":8.9144,"color":"#90e0ef"},{"demo_indicator":"35-44 years","n":15209,"pop_est":40978831,"one_in_x":2694,"log_one_in_x":7.8988,"color":"#caf0f8"},{"demo_indicator":"45-54 years","n":39077,"pop_est":42072620,"one_in_x":1077,"log_one_in_x":6.9819,"color":"#fcecc9"},{"demo_indicator":"55-64 years","n":90527,"pop_est":41756414,"one_in_x":461,"log_one_in_x":6.1334,"color":"#fb9da0"},{"demo_indicator":"65-74 years","n":150792,"pop_est":29542266,"one_in_x":196,"log_one_in_x":5.2781,"color":"#fa757c"},{"demo_indicator":"75-84 years","n":179399,"pop_est":14972513,"one_in_x":83,"log_one_in_x":4.4188,"color":"#f39237"},{"demo_indicator":"85 years and over","n":189273,"pop_est":6269017,"one_in_x":33,"log_one_in_x":3.4965,"color":"#aa2422"}]
 
 var delay = "";
 
 var h = 420;
 var hmargin = h - 10;
-var w = 500;
+var w = 1000;
 
 var svg = d3.select("body")
             .append("svg")
@@ -37,12 +28,8 @@ svg.selectAll("circle")
     .transition()
     .delay(500)
     .duration(2500)
-    .attr("cx", function(d) {
-        return d.x})
+    .attr("cx", function(d) {return d.log_one_in_x*50})
     .attr("cy", hmargin)
-    // .attr("height", function(d) {
-    // return c1bh - yScale(d.cases);
-
     
 // Animation: put them down one by one:
 // function triggerTransitionDelay(){
