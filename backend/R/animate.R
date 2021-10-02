a56d66e2a8385a1b7e2c1f712e2ee71e37a85e60
Joli Holmes <holmesjoli@gmail.com>
2021-10-02 09:44:17 -0400

    made changes

---
+++ backend/R/animate.R
@@ -0,0 +1,17 @@
+library(gapminder)
+library(ggplot2)
+library(gganimate)
+
+
+
+# Make a ggplot, but add frame=year: one image per year
+ggplot(df, aes(gdpPercap, lifeExp, size = pop, colour = country)) +
+  geom_point(alpha = 0.7, show.legend = FALSE) +
+  scale_colour_manual(values = country_colors) +
+  scale_size(range = c(2, 12)) +
+  scale_x_log10() +
+  facet_wrap(~continent) +
+  # Here comes the gganimate specific bits
+  labs(title = 'Year: {frame_time}', x = 'GDP per capita', y = 'life expectancy') +
+  transition_time(year) +
+  ease_aes('linear')
