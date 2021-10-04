b9d343979319e233351219afbd327dd3dbb87dd8
Joli Holmes <holmesjoli@gmail.com>
2021-10-04 18:39:32 -0400

    adjusted transitions and durations

--- frontend/js/transition.js
+++ frontend/js/transition.js
@@ -189,7 +189,7 @@ function buildViz(containerId) {
 }
 
 // Set the delay to 7 seconds + the animation-delay of the last text transition. Right now that is 29. Then convert to milliseconds.
-var delay = 36000;
+var delay = 32000;
 setTimeout(function() {
     buildTimer();
     buildViz("#r2");
